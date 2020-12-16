f9ad2696e01efdb9ecfc1f67837914249290f36f
gaodz <825946418@qq.com>
2020-12-16 17:32:30 +0800

    布局

--- src/pages/layout/login.vue
+++ src/pages/layout/login.vue
@@ -1,10 +1,12 @@
 <template>
   <el-container direction="vertical">
     <el-header>123123</el-header>
-    <el-aside>456</el-aside>
-    <el-main>7890
-      <img alt="Vue logo" src="../../assets/logo.png">
-    </el-main>
+    <el-container >
+      <el-aside>456</el-aside>
+      <el-main>7890
+        <img alt="Vue logo" src="../../assets/logo.png">
+      </el-main>
+    </el-container>
   </el-container>
 </template>
 <script>
